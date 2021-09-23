24a004350114d894044453d10749c8bad02a4e09
LosSantosSavior <nickrussell001@gmail.com>
2021-09-23 11:16:58 -0400

    Bank 9/23/21

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -12,7 +12,7 @@ public class Bank {
 
     }
 
-    public void doBanking()  {
+    public void doBanking() {
         var menuReader = new Scanner(System.in);
         while (true)    {
             printMenu();
@@ -23,12 +23,22 @@ public class Bank {
                 case 2:
                     addCustomer(menuReader);
                     break;
+                case 3:
+                    Customer current = selectCustomer(menuReader);
+                    break;
+                default:
+                    System.out.println("Please choose one of the menu options:");
             }
         }
     }
 
+    private Customer selectCustomer(Scanner reader) {
+
+    }
+
     private void addCustomer(Scanner inputReader) {
         System.out.print("What is the new customer's name?");
+        inputReader.nextLine(); //eat the orphan newLine from previous nextInt call
         var customerName = inputReader.nextLine();
         System.out.print("What is the new customer's tax ID (SSN)?");
         var taxID = inputReader.nextInt();
@@ -38,7 +48,7 @@ public class Bank {
 
     private void printMenu() {
         System.out.println("What would you like to do next? (select a number from the options below)");
-        System.out.println("[1] Exit the program\n[2] Add a customer\n[3] (WIP) ");
+        System.out.println("[1] Exit the program\n[2] Add a customer\n[3] Select customer by ID");
         System.out.print("Enter a choice:");
     }
 }
