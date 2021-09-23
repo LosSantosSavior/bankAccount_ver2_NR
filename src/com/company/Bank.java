7894057f9d23c541424ff28d66c87629024834ae
LosSantosSavior <nickrussell001@gmail.com>
2021-09-23 10:35:01 -0400

    Main 9/23/21

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,6 +1,38 @@
 package com.company;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank {
+    private ArrayList<bankAccount> allAccounts;
+    private ArrayList<Customer> allCustomers;
 
+    public Bank()   {
+        allAccounts = new ArrayList<bankAccount>();
+        allCustomers = new ArrayList<Customer>();
+
+    }
+
+    public void doBanking()  {
+        var menuReader = new Scanner(System.in);
+        while (true)    {
+            printMenu();
+            var userChoice = menuReader.nextInt();
+            switch (userChoice) {
+                case 1:
+                    System.exit(0);
+                case 2:
+                    addCustomer();
+                    break;
+            }
+        }
+    }
+
+    private void addCustomer() {
+
+    }
+
+    private void printMenu() {
+        System.out.println("What would you like to do next? (select a number from the options below)");
+        System.out.println("[1] Exit the program\n[2] Add a customer\n[3] (WIP) ");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var account = new bankAccount();
-        var yourAccount = new bankAccount(2000, 0.05f);
-        account.deposit(1000);
-        var newBalance = account.addInterest();
-        var balance = account.addInterest();
-
-        var succeeded = account.withdraw(2000);
-        if (succeeded)
-            System.out.println("You successfully withdrew money from your account.");
-        else
-            System.out.println("Failure to withdraw money from your account. Your balance, with the account ID "+account.getAccountID()+ ", is: " + account.checkBalance());
+        var theBank = new Bank();
+        theBank.doBanking();
     }
 }
