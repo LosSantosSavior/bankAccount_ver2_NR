01a70b2a4c04d00844b15dee9d7391bbbdf4bc9c
LosSantosSavior <nickrussell001@gmail.com>
2021-09-23 10:12:03 -0400

    Customer 9/23/21

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -6,6 +6,24 @@ public class Customer {
     private String name;
     private ArrayList<bankAccount> accounts;
 
+    public Customer(String customerName, int taxID)   {
+        customerID = taxID;
+        name = customerName;
+    }
 
+    public boolean openAccount(double initialDeposit)   {
+        var newAccount = new bankAccount();
+        newAccount.deposit(initialDeposit);
+        var didSucceed = accounts.add(newAccount);
+        return didSucceed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return customerID;
+    }
 
 }
